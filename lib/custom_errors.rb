9fc9759c1255860f2fbf15eca7bc39fe5482334d
Whittney Davis <wcdavis89@yahoo.com>
2019-12-06 00:13:15 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,6 +8,7 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class !=Person
+      begin
       raise PartnerError
         rescue PartnerError => error 
           puts error.message
